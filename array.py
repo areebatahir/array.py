de806006228bc629fc345dc171eb1738c58aee7e
areebatahir <144821163+areebatahir@users.noreply.github.com>
2023-12-12 21:20:23 +0500

    Add files via upload

---
+++ array.py
@@ -0,0 +1,24 @@
+import numpy as np
+# x = np.array([1, 2, 3, 4])
+# print(type(x))
+# print(x.ndim)
+# y = [1, 2, 3, 4]
+# print(y)
+# print(type(y))
+# a = []
+# for i in range(1,5):
+#     int_1 = int(input("Enter: "))
+#     a.append(int_1)
+# print(np.array(a))
+b = np.array([1, 2, 3, 4])
+print(b)
+print(type(b))
+print(b.ndim)
+c = np.array([[5, 6, 7, 8],[5, 6, 7, 9]])
+print(c)
+print(type(c))
+print(c.ndim)
+d = np.array([[[1, 2, 3, 4],[5, 6, 7, 8],[9, 10, 11, 12]]])
+print(d)
+print(type(d))
+print(d.ndim)
